a478d0a4b33d730cdc96f78531cf0a3c4ad126a5
melissa-koi <melissawangui3@gmail.com>
2021-04-07 00:32:43 +0300

    able to display in json after search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
+import { SearchComponent } from './components/search/search.component';
 
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
